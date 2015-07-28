4808597398d03b1a6fb1405001399ec279adb345
Ronny Eisenkolb <eisenkolb.ronny@gmail.com>
2015-07-28 13:50:36 +0200

    updated mazehall dependency to v2

--- app/app_modules/api/index.js
+++ app/app_modules/api/index.js
@@ -5,19 +5,16 @@ var projectsHandler = require('./server/handler/projects');
 
 var env = process.env.NODE_ENV || "development";
 
-module.exports = {
-    useRouting: function(app) {
+module.exports = function(app) {
+    app.use("/api", bodyParser.urlencoded({extended: false}));
+    app.use("/api", bodyParser.json());
 
-        app.use("/api", bodyParser.urlencoded({extended: false}));
-        app.use("/api", bodyParser.json());
+    app.get("/api/projects", projectsHandler.projects);
+    app.get("/api/offeredProjects", projectsHandler.offeredProjects);
 
-        app.get("/api/projects", projectsHandler.projects);
-        app.get("/api/offeredProjects", projectsHandler.offeredProjects);
+    app.post("/api/install", projectsHandler.installProject);
+    app.post("/api/start", projectsHandler.startProject);
+    app.post("/api/stop", projectsHandler.stopProject);
 
-        app.post("/api/install", projectsHandler.installProject);
-        app.post("/api/start", projectsHandler.startProject);
-        app.post("/api/stop", projectsHandler.stopProject);
-
-        app.post("/api/action", projectsHandler.projectAction);
-    }
+    app.post("/api/action", projectsHandler.projectAction);
 };
--- app/app_modules/gui/index.js
+++ app/app_modules/gui/index.js
@@ -3,8 +3,6 @@ var serveStatic = require("serve-static");
 
 var env = process.env.NODE_ENV || "development";
 
-module.exports = {
-    useRouting: function(app) {
-        app.use(serveStatic(path.join(__dirname, env === "development" ? "public/src" : "public/dist")));
-    }
+module.exports = function(app) {
+    app.use(serveStatic(path.join(__dirname, env === "development" ? "public/src" : "public/dist")));
 };
