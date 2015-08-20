2b68d435c775f8d687d9499202b1cd081b01f1b5
Ronny Eisenkolb <eisenkolb.ronny@gmail.com>
2015-08-20 18:56:20 +0200

    Changed asar unpack rules and small bugfixing in OSX release

--- tasks/release_linux.js
+++ tasks/release_linux.js
@@ -35,7 +35,7 @@ var packageBuiltApp = function () {
     var deferred = Q.defer();
 
     asar.createPackageWithOptions(projectDir.path('build'), readyAppDir.path('resources/app.asar'), {
-        unpack: "{main.js,*.*}"
+        unpack: "!**/node_modules/**"
     }, function() {
         deferred.resolve();
     });
