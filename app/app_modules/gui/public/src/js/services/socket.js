187ac2f64d1647127da055500aa7e00b1646019f
Ronny Eisenkolb <eisenkolb.ronny@gmail.com>
2015-09-14 11:57:33 +0200

    revert socket.js changes from #42ebf0f5844

--- app/app_modules/gui/public/src/js/services/socket.js
+++ app/app_modules/gui/public/src/js/services/socket.js
@@ -186,8 +186,8 @@ angular.module('eintopf.services.socket.states', [])
     var containerList_ = Kefir.fromEvent(socket, 'res:containers:list')
       .toProperty();
     reqContainersList.emit();
-    //Kefir.fromPoll(2000, reqContainersList.emit)
-    //  .onValue(function() {});
+    Kefir.fromPoll(2000, reqContainersList.emit)
+      .onValue(function() {});
     return containerList_;
   }])
 
@@ -203,8 +203,8 @@ angular.module('eintopf.services.socket.states', [])
     var appList_ = Kefir.fromEvent(socket, 'res:apps:list')
       .toProperty();
     reqAppsList.emit();
-    //Kefir.fromPoll(2000, reqAppsList.emit)
-    //  .onValue(function() {});
+    Kefir.fromPoll(2000, reqAppsList.emit)
+      .onValue(function() {});
     return appList_;
   }])
 
