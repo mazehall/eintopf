8b045206d708720beebc626ea1592d269a382036
Marcel Kilian <cdskilian@gmail.com>
2015-09-23 13:49:53 +0200

    eintopf-17 BUG: occasional 'Cannot get /' Error on app start

--- app/server.js
+++ app/server.js
@@ -1,8 +1,21 @@
+var mazehall = require('mazehall');
 var server = require('./app.js');
+
+var guiLoaded = false;
+var serverStarted = false;
+
+mazehall.moduleStream.onValue(function(val) {
+  if(val.module != 'gui') return false;
+
+  guiLoaded = true;
+  if(serverStarted) process.emit('app:serverstarted'); //emit server start when server already listens
+});
 
 process.on('app:startserver', function(port) {
   server.listen(port, function() {
-    process.emit('app:serverstarted');
     console.log('server listen on port: ' + port);
+
+    serverStarted = true;
+    if(guiLoaded) process.emit('app:serverstarted'); //emit server start when gui module was already loaded
   });
 });
