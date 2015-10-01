296ad548c176439fbd2ad3002cefc11350599646
Marcel Kilian <cdskilian@gmail.com>
2015-10-01 17:12:32 +0200

    project cleanup: - moved the only used function from the api module into gui - removed api mazehall module - removed unused socket event 'projects:list:refresh'

--- app/app_modules/gui/index.js
+++ app/app_modules/gui/index.js
@@ -1,12 +1,16 @@
 var path = require("path");
 var serveStatic = require("serve-static");
 var _r = require("kefir");
+
 var statesSocket = require("./server/states.coffee");
+var handlerModel =  require("../../models/handler/index.coffee");
 
 var env = process.env.NODE_ENV || "development";
 
 module.exports = function(app) {
   app.use(serveStatic(path.join(__dirname, env === "development" ? "public/src" : "public/dist")));
+
+  app.get("/projects/:project/:resource", handlerModel.projectResource);
 
   var socketServer = app.get('io').of('/states');
   var sockets_ = _r.fromEvents(
