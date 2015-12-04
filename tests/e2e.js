c400ffe326143c89a10b90e883b21cec1b552ac5
Ronny Eisenkolb <eisenkolb.ronny@gmail.com>
2015-12-04 16:23:23 +0100

    Removed chromedriver check for e2e test

--- tests/e2e.js
+++ tests/e2e.js
@@ -7,33 +7,11 @@ try {
     fs.statSync("./node_modules/chromedriver");
     fs.statSync("./node_modules/selenium-server-standalone-jar");
 } catch(e) {
-    var options = ["install", "chromedriver@2.20.0", "nightwatch@0.8.8", "selenium-server-standalone-jar@2.47.1"];
+    var options = ["install", "chromedriver@2.20.0", "nightwatch@^0.8.8", "selenium-server-standalone-jar@2.47.1"];
     var install = spawn(process.platform === "win32" ? "npm.cmd" : "npm", options, {
         env: process.env,
         stdio: "inherit"
     });
-}
-
-var chromebinarylocation;
-switch (process.platform) {
-    case 'darwin':
-        chromebinarylocation = "/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome";
-        break;
-    case 'linux':
-        chromebinarylocation = "/usr/bin/google-chrome";
-        break;
-    case 'win32':
-        chromebinarylocation = "C:\\Users\\%USERNAME%\\AppData\\Local\\Google\\Chrome\\Application\\chrome.exe".replace("%USERNAME%", process.env.username);
-        break;
-}
-
-try {
-    fs.statSync(chromebinarylocation);
-} catch(e) {
-    console.log("ChromeDriver requires the default Chrome install location and can not be changed:");
-    console.log("  ->", chromebinarylocation, "\n");
-    console.log("Install Chrome or Eintopf at this location or link the binary");
-    process.exit(0);
 }
 
 if (process.platform === "darwin") {
